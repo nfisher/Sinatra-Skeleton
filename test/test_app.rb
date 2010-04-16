34c6366b2e2c0ff98349b62e0669e9b7e1b999bb
Nathan Fisher <nfisher@junctionbox.ca>
2010-04-17 00:25:40 +0100

    Introduce rack's test suite.

--- test/test_app.rb
+++ test/test_app.rb
@@ -2,8 +2,17 @@ $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
 require 'app'
 require 'test/unit'
+require 'rack/test'
+
+set :environment, :test
 
 class AppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+	def app
+		App
+	end
+
   def test_fail
     flunk 'Write your App tests!'
   end
